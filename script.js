62192cfdb9621db0dd6bda0d1f05310cc17e9010
WendeVoordes <113184312+WendeVoordes@users.noreply.github.com>
2023-10-05 10:45:54 +0000

    Begin-eindopdracht

--- script.js
+++ script.js
@@ -1,81 +1,147 @@
-class Bal {
-  constructor(x,y) {
-  this.diameter = 40;
-  this.straal = 20;
-  this.x = x;
-  this.y = y;
-  this.R = random(0,255);
-  this.G = random(0,255);
-  this.B = random(0,255);
-  this.alpha = 0.8;
-  this.snelheid = 0;
-  this.aantalKerenGestuiterd = 0;
-  this.geraakt = false;
+class Raster {
+  constructor(r,k) {
+    this.aantalRijen = r;
+    this.aantalKolommen = k;
+    this.celGrootte = null;
   }
   
-  beweeg() {
-    this.snelheid+=9.81/15;
-    this.y += this.snelheid;
-    if (this.y >= canvas.height - this.straal) {
-      this.snelheid *= -1;
-      this.y=canvas.height - this.straal;
-      this.aantalKerenGestuiterd++;
-    }
+  berekenCelGrootte() {
+    this.celGrootte = canvas.width / this.aantalKolommen;
   }
   
   teken() {
     push();
-    noStroke();
-    fill(this.R,this.G,this.B,this.alpha);
-    ellipse(this.x,this.y,this.diameter);
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
     pop();
   }
 }
 
-var ballenArray = [];
-var erIsNogGeenBalGestuiterd = true;
-var score = 0;
+class Jos {
+  constructor() {
+    this.x = 400;
+    this.y = 300;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null;
+    this.gehaald = false;
+  }
+  
+  beweeg() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+    
+    this.x = constrain(this.x,0,canvas.width);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+    
+    if (this.x == canvas.width) {
+      this.gehaald = true;
+    }
+  }
+  
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}  
+
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+  
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+}
 
 function setup() {
-  canvas = createCanvas(450,450);
+  canvas = createCanvas(900,600);
   canvas.parent();
-  colorMode(RGB,255,255,255,1);
-  frameRate(15);
+  frameRate(10);
   textFont("Verdana");
-  textSize(24);
-  ballenArray[0] = new Bal(225,50);
+  textSize(90);
+  
+  raster = new Raster(6,9);
+  
+  raster.berekenCelGrootte();
+  
+  eve = new Jos();
+  eve.stapGrootte = 1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
+  }
+  
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
 
+  bob = new Vijand(600,400);
+  bob.stapGrootte = 1*eve.stapGrootte;
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");  
 }
 
 function draw() {
-  background('salmon');
-  text("N:"+ballenArray.length+" score:"+score,5,24);
-  if (mouseIsPressed && erIsNogGeenBalGestuiterd) {
-    ballenArray.push(new Bal(mouseX,mouseY));
+  background(brug);
+  raster.teken();
+  eve.beweeg();
+  alice.beweeg();
+  bob.beweeg();
+  eve.toon();
+  alice.toon();
+  bob.toon();
+  
+  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
+    noLoop();
   }
   
-  for (var b = 0;b < ballenArray.length;b++) {
-    ballenArray[b].beweeg();
-    ballenArray[b].teken();
-    if (ballenArray[b].aantalKerenGestuiterd == 1) {
-      erIsNogGeenBalGestuiterd = false;
-      if (dist(mouseX,mouseY,ballenArray[b].x,ballenArray[b].y) <= ballenArray[b].straal && mouseIsPressed && ballenArray[b].geraakt == false) {
-        score++;
-        ballenArray[b].geraakt = true;
-        ballenArray[b].alpha = 0;
-      }
-    }
-    if (ballenArray[b].aantalKerenGestuiterd == 2) {
-      if (ballenArray.length == score) {
-        background('green');
-        text("Het is je gelukt! Je score is "+score+" :)",5,24);
-        noLoop();
-      }
-      else {
-        background('red');
-        text("Helaas. Je hebt verloren :(",5,24);
-        noLoop();
-      }
-    }
+  if (eve.gehaald) {
+    background('green');
+    fill('white');
+    text("Je hebt gewonnen!",30,300);
+    noLoop();
   }
 }
